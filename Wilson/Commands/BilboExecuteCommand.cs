4fa7409bb424d79fdfbecb625ca542e386a3db18
owentrokebillard <owentrokebillard@nureva.com>
2016-07-28 16:29:07 -0600

    Added Source

---
+++ Wilson/Commands/BilboExecuteCommand.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wilson.ViewModels;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace Wilson.Commands
+{
+    public class BilboExecuteCommand : ICommand
+    {
+        private BilboViewModel _viewModel;
+
+        public BilboExecuteCommand(BilboViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public async void Execute(object parameter)
+        {
+            XDocument n;
+
+            var script = _viewModel.ScriptText;
+            var scriptOptions = ScriptOptions.Default
+                .AddImports(
+                    "System",
+                    "System.Text",
+                    "System.Collections.Generic"
+                );
+
+            try
+            {
+                var result = await CSharpScript.EvaluateAsync(script, scriptOptions);
+                _viewModel.ResultText = result?.ToString();
+            }
+            catch (Exception ex)
+            {
+                _viewModel.ResultText = "Error";
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ Wilson/ViewModels/BilboViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wilson.Commands;
+
+namespace Wilson.ViewModels
+{
+    public class BilboViewModel : INotifyPropertyChanged
+    {
+        private string _scriptText;
+        private string _resultText;
+
+        public string ScriptText
+        {
+            get { return _scriptText; }
+            set
+            {
+                _scriptText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ResultText
+        {
+            get { return _resultText; }
+            set
+            {
+                _resultText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand BilboExecuteCommand { get; set; }
+
+        public BilboViewModel()
+        {
+            ScriptText = string.Empty;
+
+            BilboExecuteCommand = new BilboExecuteCommand(this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
